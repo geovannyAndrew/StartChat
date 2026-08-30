cf6b7cc04f9b3f88ac9b71e2d338ec850af4b7a7
Geovanny Buitrago <geovanny.andrew911@gmail.com>
2026-08-30 11:28:48 -0500

    enhancement: Remove unused Colors and add an error Log.

--- app/src/androidMain/kotlin/com/gyros/startchat/data/AppInfoImpl.kt
+++ app/src/androidMain/kotlin/com/gyros/startchat/data/AppInfoImpl.kt
@@ -3,6 +3,7 @@ package com.gyros.startchat.data
 import android.content.Context
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import android.util.Log
 
 class AppInfoImpl(
     private val context: Context
@@ -14,6 +15,7 @@ class AppInfoImpl(
                 context.packageManager.getPackageInfo(context.packageName, 0)
             packageInfo.versionName ?: ""
         } catch (e: PackageManager.NameNotFoundException) {
+            Log.e("AppInfo", "Error getting app version", e)
             ""
         }
     }
